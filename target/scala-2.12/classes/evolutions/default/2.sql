506fa1d1fe61705f784e4c2435bd554977a6d33a
Ciniflix <eoin22199@gmail.com>
2018-04-24 09:50:08 +0100

    final

--- target/scala-2.12/classes/evolutions/default/2.sql
+++ target/scala-2.12/classes/evolutions/default/2.sql
@@ -8,26 +8,27 @@ DELETE FROM USER;
 
 insert into category (id,name) values ( 1,'Mens' );
 insert into category (id,name) values ( 2,'Womens' );
-insert into category (id,name) values ( 3,'Computer' );
-insert into category (id,name) values ( 4,'Furniture' );
-insert into category (id,name) values ( 5,'EOIn' );
-insert into category (id,name) values ( 6,'Laundry' );
-insert into category (id,name) values ( 7,'Mobile Phone' );
-insert into category (id,name) values ( 8,'Television' );
+insert into category (id,name) values ( 3,'Kids' );
+insert into category (id,name) values ( 4,'Unisex' );
 
-insert into product (id,category_id,name,description,stock,price) values ( 1,5,'Kettle','Steel Electric Kettle',100,55.00 );
-insert into product (id,category_id,name,description,stock,price) values ( 2,5,'Fridge freezer','Fridge + freezer large',45,799.00 );
-insert into product (id,category_id,name,description,stock,price) values ( 3,1,'Portable Music Player','250GB music player (MP3,MP4,WMA,WAV)',5,99.00 );
-insert into product (id,category_id,name,description,stock,price) values ( 4,3,'13inch Laptop','HP laptop,8GB RAM,250GB SSD',45,799.00 );
-insert into product (id,category_id,name,description,stock,price) values ( 5,3,'8inch Tablet','Android 5.1 Tablet,32GB storage,8inch screen',5,99.00 );
-insert into product (id,category_id,name,description,stock,price) values ( 6,8,'60inch TV','Sony 4K,OLED,Smart TV',12,5799.00 );
-insert into product (id,category_id,name,description,stock,price) values ( 7,6,'Washing Machine','1600rpm spin,A+++ rated,10KG',50,699.00 );
-insert into product (id,category_id,name,description,stock,price) values ( 8,7,'Phone','Windows 10,5.2inch OLED,3GB RAM,64GB Storage',45,799.00 );
-insert into product (id,category_id,name,description,stock,price) values ( 9,3,'10inch Tablet','Windows 10,128GB storage,8inch screen',5,299.00 );
-insert into product (id,category_id,name,description,stock,price) values ( 10,5,'Oven','Oven + Grill,Stainless Steel',10,399.00 );
-insert into product (id,category_id,name,description,stock,price) values ( 11,4,'Bed','Super King size,super comfort mattress',5,899.00 );
-insert into product (id,category_id,name,description,stock,price) values ( 12,2,'Learning JavaScript','Become a JavaScript expert in 2 hours!',50,29.00 );
 
-insert into user (email,name,password,role) values ( 'admin@products.com', 'Alice Admin', 'password', 'admin' );
-insert into user (email,name,password,role) values ( 'manager@products.com', 'Bob Manager', 'password', 'manager' );
-insert into user (email,name,password,role) values ( 'customer@products.com', 'Charlie Customer', 'password', 'customer' );
+insert into product (id,category_id,name,description,stock,price) values ( 1,4,'Vans','Black and white old skool trainers',25,72.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 2,4,'Converse','All star all white mono Ox',32,75.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 3,1,'Nike','All White VapourMax',20,180.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 4,2,'Adidias','Pink n-5923 modelled after vintage running shoes',12,56.95 );
+insert into product (id,category_id,name,description,stock,price) values ( 5,3,'Nike','White air force 1 youth trainers',28,63.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 6,3,'New Balance','Black & Red 247 junior trainers',33,44.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 7,1,'Vans','Old SKoool black fabric with flame overlays ',10,74.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 8,1,'Nike SB','Burgundy check solarsoft trainers',14,60.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 9,2,'Reebok','Dark grey classic leather tonal nbk trainers',22,55.95 );
+insert into product (id,category_id,name,description,stock,price) values ( 10,4,'Adidas','White Stan Smith with black stripes ',34,100.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 11,3,'Puma','Suede Heart Jewel girls Trainers',1,60.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 12,2,'Nike','Khaki air max 270 boy youth trainers',32,106.00 );
+
+insert into user (email,name,password,role) values ( 'admin@products.com', 'Ethan Admin', 'password', 'admin' );
+insert into user (email,name,password,role) values ( 'manager@products.com', 'Eoin Manager', 'password', 'manager' );
+insert into user (email,name,password,role) values ( 'customer@products.com', 'Reece Customer', 'password', 'customer' );
+
+
+
+
